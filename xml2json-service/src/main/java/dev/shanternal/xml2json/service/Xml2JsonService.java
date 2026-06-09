ad5c269a3c3411d16eeb4d63dc5df3674fac73fa
Semyon Bakhmatov <shanternal22@gmail.com>
2026-06-09 12:08:41 +0300

    feat: implement XML to JSON conversion

--- xml2json-service/src/main/java/dev/shanternal/xml2json/service/Xml2JsonService.java
+++ xml2json-service/src/main/java/dev/shanternal/xml2json/service/Xml2JsonService.java
@@ -1,10 +1,32 @@
 package dev.shanternal.xml2json.service;
 
+import dev.shanternal.xml2json.exception.InvalidXmlException;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class Xml2JsonService {
+
     public String convert(String xml) {
-        return "";
+
+        if (!StringUtils.hasText(xml)) {
+            throw new InvalidXmlException("XML is empty");
+        }
+
+        try {
+            JSONObject json = XML.toJSONObject(xml);
+
+            if (json.isEmpty()) {
+                throw new InvalidXmlException("Invalid XML");
+            }
+
+            return json.toString();
+
+        } catch (JSONException e) {
+            throw new InvalidXmlException("Invalid XML", e);
+        }
     }
 }
