ec2ba28fa26d75ef309433da10d7d66d648e2f17
Semyon Bakhmatov <shanternal22@gmail.com>
2026-06-09 11:59:34 +0300

    feat: add global exception handler

---
+++ xml2json-service/src/main/java/dev/shanternal/xml2json/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package dev.shanternal.xml2json.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleInternalServerError(Exception e) {
+        return buildProblemDetail(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Internal Server Error",
+                "An unexpected error occurred");
+    }
+
+    private ProblemDetail buildProblemDetail(HttpStatus status, String title, String detail) {
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(status, detail);
+        problem.setTitle(title);
+        problem.setProperty("timestamp", Instant.now());
+        return problem;
+    }
+}
