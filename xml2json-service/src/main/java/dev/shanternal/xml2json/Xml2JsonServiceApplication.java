cebb5383395026c822dc22be7657c58867e60842
Semyon Bakhmatov <shanternal22@gmail.com>
2026-06-09 11:21:03 +0300

    chore: initialize xml2json service

---
+++ xml2json-service/src/main/java/dev/shanternal/xml2json/Xml2JsonServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.shanternal.xml2json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xml2JsonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xml2JsonServiceApplication.class, args);
+	}
+
+}
---
+++ xml2json-service/src/test/java/dev/shanternal/xml2json/Xml2JsonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shanternal.xml2json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xml2JsonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
