8803f55a500c4bcf0a6d053062de018c2c2e2e8e
Semyon Bakhmatov <shanternal22@gmail.com>
2026-06-11 10:37:02 +0300

    feat: add ConversionResultRepository

---
+++ request-service/src/main/java/dev/shanternal/request/repository/ConversionResultRepository.java
@@ -0,0 +1,48 @@
+package dev.shanternal.request.repository;
+
+import dev.shanternal.request.entity.ConversionResult;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ConversionResultRepository {
+
+    private final SessionFactory sessionFactory;
+
+    public ConversionResult saveIfAbsent(ConversionResult entity) {
+        Session session = sessionFactory.getCurrentSession();
+
+        //TODO: При переходе на Postgres 19 заменить на ON CONFLICT (...) DO SELECT (...)
+        ConversionResult inserted = session.createNativeQuery("""
+                    INSERT INTO conversion_results
+                        (xml_hash, canonical_xml, xml_tags_count, target_json, json_keys_count)
+                    VALUES
+                        (:xmlHash, :canonicalXml, :xmlTagsCount, :targetJson, :jsonKeysCount)
+                    ON CONFLICT (xml_hash) DO NOTHING
+                    RETURNING *
+                    """, ConversionResult.class)
+                .setParameter("xmlHash", entity.getXmlHash())
+                .setParameter("canonicalXml", entity.getCanonicalXml())
+                .setParameter("xmlTagsCount", entity.getXmlTagsCount())
+                .setParameter("targetJson", entity.getTargetJson())
+                .setParameter("jsonKeysCount", entity.getJsonKeysCount())
+                .getSingleResultOrNull();
+
+        if (inserted == null) {
+            inserted = session.get(ConversionResult.class, entity.getXmlHash());
+        }
+
+        return inserted;
+    }
+
+    public Optional<ConversionResult> findByXmlHash(String xmlHash) {
+        return Optional.ofNullable(
+                sessionFactory.getCurrentSession().get(ConversionResult.class, xmlHash)
+        );
+    }
+}
