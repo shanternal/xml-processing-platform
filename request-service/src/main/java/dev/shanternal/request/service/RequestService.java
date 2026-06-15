0beec2484dcacdddee8e4e3b237f5ee52145c360
Semyon Bakhmatov <shanternal22@gmail.com>
2026-06-15 09:47:28 +0300

    feat: implement request pagination in service

--- request-service/src/main/java/dev/shanternal/request/service/RequestService.java
+++ request-service/src/main/java/dev/shanternal/request/service/RequestService.java
@@ -5,17 +5,24 @@ import dev.shanternal.request.dto.response.ConvertXmlResult;
 import dev.shanternal.request.dto.response.Page;
 import dev.shanternal.request.dto.response.ProcessedRequestDetail;
 import dev.shanternal.request.dto.response.ProcessedRequestSummary;
+import dev.shanternal.request.repository.ProcessedRequestRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class RequestService {
+
+    private final ProcessedRequestRepository processedRequestRepository;
 
     public ConvertXmlResult convertXml(String xml) {
         return null;
     }
 
+    @Transactional(readOnly = true)
     public Page<ProcessedRequestSummary> getPage(ProcessedRequestFilter filter, int page, int size) {
-        return null;
+        return processedRequestRepository.findAll(filter, page, size);
     }
 
     public ProcessedRequestDetail getById(Long id) {
