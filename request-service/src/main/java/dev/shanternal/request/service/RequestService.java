a956c2fe5c2ed9947a8ceb2981c5e7951aea5656
Semyon Bakhmatov <shanternal22@gmail.com>
2026-06-15 10:37:04 +0300

    feat: implement processed request retrieval by id

--- request-service/src/main/java/dev/shanternal/request/service/RequestService.java
+++ request-service/src/main/java/dev/shanternal/request/service/RequestService.java
@@ -5,6 +5,9 @@ import dev.shanternal.request.dto.response.ConvertXmlResult;
 import dev.shanternal.request.dto.response.Page;
 import dev.shanternal.request.dto.response.ProcessedRequestDetail;
 import dev.shanternal.request.dto.response.ProcessedRequestSummary;
+import dev.shanternal.request.entity.ProcessedRequest;
+import dev.shanternal.request.exception.ResourceNotFoundException;
+import dev.shanternal.request.mapper.RequestMapper;
 import dev.shanternal.request.repository.ProcessedRequestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class RequestService {
 
     private final ProcessedRequestRepository processedRequestRepository;
+    private final RequestMapper requestMapper;
 
     public ConvertXmlResult convertXml(String xml) {
         return null;
@@ -25,7 +29,11 @@ public class RequestService {
         return processedRequestRepository.findAll(filter, page, size);
     }
 
+    @Transactional(readOnly = true)
     public ProcessedRequestDetail getById(Long id) {
-        return null;
+        ProcessedRequest processedRequest = processedRequestRepository.findByIdWithConversion(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Processed request with id=%d not found".formatted(id)));
+
+        return requestMapper.toProcessedRequestDetail(processedRequest);
     }
 }
